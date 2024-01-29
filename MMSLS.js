ea3cc0a408c4fc1a561d09a323411536d7e76e8d
TGProw <101246667+TGProw@users.noreply.github.com>
2024-01-29 22:50:13 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=32454734&pid=efb3c256029b3b2c05b34c3cdba56e53&tid=1b8c445e5cfc9fef538fb30799f290e7",
+    "https://www.maimemo.com/share/page?uid=24047718&pid=1030b3649ab488920f1eef658ec56a15&tid=5b10b37f4cff9138aa27e4a6f4d8b318",
 
 
   ]
